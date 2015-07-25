3e490e0760c78d7063d0816b16facf81692cbafa
David OKeefe <davidpokeefe3@gmail.com>
2015-07-25 17:15:51 -0400

    Added params to blogs controller
    * defined private method blogs_params
    * updated create method to create using blog_params

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,6 +7,13 @@ class BlogsController < ApplicationController
   end
 
   def create
+    Blog.create(blog_params)
     redirect_to blogs_path
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:name, :description)
   end
 end
